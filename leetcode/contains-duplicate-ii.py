31b908837e6b1203ef6056be1e0daa6a31cb125a
Ashan Prapaharan <47048522+apraba05@users.noreply.github.com>
2026-01-29 17:56:11 -0500

    Update solution for contains-duplicate-ii

--- leetcode/contains-duplicate-ii.py
+++ leetcode/contains-duplicate-ii.py
@@ -1,14 +1,13 @@
 from collections import defaultdict
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        nums = defaultdict(int)
-        arr = []
-        for i in range len(nums):
-            nums[nums[i]].append(i)
-            if (len nums[nums[i]].value() > 1):
-                arr.append(nums.value())
-                
-        if (arr[0] == arr[1] and abs(arr[0] - arr[1] <= k)):
-            return True
+        new_nums = defaultdict(list)
+        for i in range (len(nums)):
+            new_nums[nums[i]].append(i)
+            if (len (new_nums[nums[i]]) > 1):
+                new_var = new_nums[nums[i]]
+        print(new_var)
+        if (nums[new_var[0]] == nums[new_var[1]] and abs(new_var[0] - new_var[1] <= k)):
+             return True
         
         return False
