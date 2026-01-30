2e1f09a228722d9440ee9b2bc907008710fe15fb
Ashan Prapaharan <47048522+apraba05@users.noreply.github.com>
2026-01-29 19:29:09 -0500

    Add solution for top-k-frequent-elements

---
+++ leetcode/top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(list)
+        another_one = defaultdict(int)
+        arr = [0]*k
+
+        for i in range (len(nums)):
+            freq[nums[i]].append(i)
+            
+        for i in freq:
+            another_one[i] = len(freq[i])
+        
+        sorted_another_one = sorted(another_one.items(), key=lambda x:x[1], reverse=True)
+        new_sort = dict(sorted_another_one)
+
+        for i in range (len(arr)):
+            
+
+        print(arr)
