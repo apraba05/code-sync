f92cfc6ed526b5665681e1145e2462a2e1060f11
Ashan Prapaharan <47048522+apraba05@users.noreply.github.com>
2026-01-31 18:00:38 -0500

    Update solution for daily-temperatures

--- leetcode/daily-temperatures.py
+++ leetcode/daily-temperatures.py
@@ -1,60 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        days = []
-        stack = []
+        n = len(temperatures)
+        days = [0] * n
+        stack = []  # will store indices
 
-        for t in temperatures:
-            stack.append(t)   
-        stack.reverse()
-
-        while (len(stack) > 0):
-            start_len = len(days)
-            if (len(stack) == 1):
-                stack.pop()
-                days.append(0)
-                continue
-
-            last = len(stack) - 1
-            second_last = len(stack) - 2
-
-            if(len(stack) == 2):
-                if(stack[1] < stack[0]):
-                    stack.pop()
-                    days.append(1)
-                else:
-                    stack.pop()
-                    days.append(0)
-                continue
-            
-         
-            if (stack[last] < stack[second_last]):
-                    diff = last - second_last
-                    stack.pop()
-                    days.append(diff)
-            elif (stack[last] > stack[second_last]):
-                    found = False
-                    last = len(stack) - 1
-                    second_last = len(stack) - 2
-                    while(second_last >= 0):
-                        if(stack[last] < stack[second_last]):
-                            diff = last - second_last
-                            print(diff)
-                            stack.pop()
-                            days.append(diff)
-                            found = True
-                            break
-                        else:
-                            second_last -= 1
-
-                    if not found:
-                        stack.pop()
-                        days.append(0)
-                    continue
-
-
-            else:
-                    stack.pop()
-                    days.append(0)
-                    continue
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                j = stack.pop()
+                days[j] = i - j
+            stack.append(i)
 
         return days
