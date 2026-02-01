dca1f1f4fbff63e80e615c19a783c122627c793c
Ashan Prapaharan <47048522+apraba05@users.noreply.github.com>
2026-02-01 13:20:39 -0500

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,12 +1,17 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prev = {}
-        for i,x in enumerate(nums):
-            prev[x] = i
+        numsMap = defaultdict()
+        indices = []
+
+        for i in range (len(nums)):
+            numsMap[nums[i]] = target - nums[i]
+            if(numsMap[nums[i]] in nums):
+                if(numsMap[nums[i]] == nums[i]):
+                    continue
+                else:
+                    indices.append(i)
+            else:
+                continue
         
-        for i,x in enumerate(nums):
-            diff = target - x
-            if diff in prev and prev[diff] != i:
-                return[i,prev[diff]]
-        
-        return[]
+        return indices
