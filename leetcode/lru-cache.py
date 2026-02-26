bab23d5a6547b1dac0db38459d3d047a068a9413
Ashan Prapaharan <47048522+apraba05@users.noreply.github.com>
2026-02-26 10:06:47 -0500

    Update solution for lru-cache

--- leetcode/lru-cache.py
+++ leetcode/lru-cache.py
@@ -4,22 +4,33 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dict = OrderedDict()
+        self.dictionary = OrderedDict()
+       
         
 
     def get(self, key: int) -> int:
-        if key not in self.dict:
+        if key not in self.dictionary:
             return -1
-        self.dict.move_to_end(key)
-        return self.dict[key]
+        else: 
+            self.dictionary.move_to_end(key)
+            return self.dictionary[key]
+
+      
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            self.dict.move_to_end(key)
-        self.dict[key] = value
-        if len(self.dict) > self.capacity:
-            self.dict.popitem(last=False)
+        
+        if key in self.dictionary:
+           self.dictionary.move_to_end(key)
+        self.dictionary[key] = value
+        
+        if (self.capacity < len(self.dictionary)):
+            self.dictionary.popitem(last=False)
+        
+
+
+
+        
         
 
 
